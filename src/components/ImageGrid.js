85a2a04c3dc19914f65855aea5dd330b0bc40ac6
Nurlan <nurlanimamali@gmail.com>
2024-01-03 12:39:01 +0100

    Grid are used for columns

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,34 +1,44 @@
 import React from "react";
-import { ImageList, ImageListItem } from "@mui/material";
+import { ImageListItem, Grid, Paper } from "@mui/material";
 import { motion, AnimatePresence } from "framer-motion";
 
 const ImageListComponent = ({ images, handleImageClick }) => {
   return (
     <AnimatePresence>
-      <ImageList variant="woven" cols={3} gap={10}>
+      <Grid container spacing={2}>
         {images.map((image, index) => (
-          <motion.div
-            key={image.id}
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            style={{ display: "inline-block", margin: "10px", cursor: "pointer" }}
-            whileHover={{ scale: 1.1 }}
-          >
-            <ImageListItem
-              key={image.id}
-              onClick={() => handleImageClick(image.urls.full)}
+          <Grid key={image.id} item xs={12} sm={6} md={4}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              style={{
+                display: "inline-block",
+                margin: "10px",
+                cursor: "pointer",
+              }}
+              whileHover={{ scale: 1.1 }}
             >
-              <img
-                src={image.urls.regular}
-                alt={image.alt_description}
-                
-              />
-            </ImageListItem>
-          </motion.div>
+              <ImageListItem
+                component={Paper} // Use Paper for a card-like appearance
+                sx={{
+                  cursor: "pointer",
+                  aspectRatio: "1", // Set a square aspect ratio for the images
+                }}
+                key={image.id}
+                onClick={() => handleImageClick(image.urls.full)}
+              >
+                <img
+                  src={image.urls.regular}
+                  alt={image.alt_description}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              </ImageListItem>
+            </motion.div>
+          </Grid>
         ))}
-      </ImageList>
+      </Grid>
     </AnimatePresence>
   );
 };
